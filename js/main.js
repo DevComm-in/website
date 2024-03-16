762bfa7cd2f7da904fc9d9c5b87c6c131e851b81
Subhash91134857 <Subhashpanday58@gmail.com>
2024-03-16 19:20:18 +0530

    intial commit

---
+++ js/main.js
@@ -0,0 +1,94 @@
+$(document).ready(function(){
+
+     $('.fa-bars').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $(window).on('load scroll',function(){
+        $('.fa-bars').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+
+        if($(window).scrollTop()>35)
+        {
+            $('.header').css({'background':'#002e5f','box-shadow':'0 .2rem .5rem rgba(0,0,0,.4)'});
+        }
+        else
+        {
+            $('.header').css({'background':'none','box-shadow':'none'});
+        }
+    });
+
+    const counters = document.querySelectorAll('.counter');
+    const speed = 1;
+    counters.forEach(counter => {
+	const updateCount = () => {
+		const target = +counter.getAttribute('data-target');
+		const count = +counter.innerText;
+		const inc = target / speed;
+		if (count < target) {
+			counter.innerText = count + inc;
+			setTimeout(updateCount, 1);
+		} else {
+			counter.innerText = target;
+		}
+	};
+	  updateCount();
+   });
+
+   (function ($) {
+    "use strict";
+    
+    $(".clients-carousel").owlCarousel({
+        autoplay: true,
+        dots: true,
+        loop: true,
+        responsive: { 0: {items: 2}, 768: {items: 4}, 900: {items: 6} }
+    });
+
+    $(".testimonials-carousel").owlCarousel({
+        autoplay: true,
+        dots: true,
+        loop: true,
+        responsive: { 0: {items: 1}, 576: {items: 2}, 768: {items: 3}, 992: {items: 4} }
+    });
+    
+})(jQuery);
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+        $('.back-to-top').fadeIn('slow');
+    } else {
+        $('.back-to-top').fadeOut('slow');
+    }
+});
+$('.back-to-top').click(function () {
+    $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+    return false;
+});
+
+$('.accordion-header').click(function(){
+    $('.accordion .accordion-body').slideUp(500);
+    $(this).next('.accordion-body').slideDown(500);
+    $('.accordion .accordion-header span').text('+');
+    $(this).children('span').text('-');
+});
+
+});
+
+// for taking the input of contact form
+document.getElementById("contactForm").addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the form from submitting
+
+    // Retrieve input values
+    var name = document.getElementById("name").value;
+    var phone = document.getElementById("phone").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+
+    // Log input values to the console
+    console.log("Name:", name);
+    console.log("Phone:", phone);
+    console.log("Email:", email);
+    console.log("Message:", message);
+})
